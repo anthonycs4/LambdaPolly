c72749482f6c8fccbcbf8dba0a74e2e3da33acd0
anthonycs4 <ac2020067573@virtual.upt.pe>
2024-05-28 10:21:27 -0500

    Flutter

--- lambdapolly.py
+++ lambdapolly.py
@@ -6,7 +6,7 @@ import json
 text = input("Enter the text to synthesize: ")
 
 # URL of the Polly API with the text as a parameter
-api_url = f"https://56q2rxpufj.execute-api.us-east-2.amazonaws.com/prod/synthesize?text={text}"
+api_url = f"https://2k2pcb479g.execute-api.us-east-2.amazonaws.com/prod/?text={text}"
 
 # Make a GET request to the API
 response = requests.get(api_url)
@@ -17,11 +17,16 @@ if response.status_code == 200:
     response_data = response.json()
 
     # Get the audio content in base64
-    audio_base64 = response_data.get('audioContent')
+    audio_base64 = response_data.get('body')
+    body_json = json.loads(audio_base64)
+    audio_base64_value = body_json.get('audio_base64')
 
-    if audio_base64:
+    # Print the base64 audio content
+    print("Base64 audio content:", audio_base64_value)
+
+    if audio_base64_value:
         # Decode the audio content
-        audio_data = base64.b64decode(audio_base64)
+        audio_data = base64.b64decode(audio_base64_value)
 
         # Save the audio to a file
         with open("output.mp3", "wb") as audio_file:
